c7729d6199dbda79fa91b1335df6b3dd486065e9
Александр Волкович <volkovich@MacBook-Air-Aleksandr.local>
2022-06-12 21:41:16 +0300

    Add digitssort.py

--- codewars/digitssort.py
+++ codewars/digitssort.py
@@ -1,6 +1,10 @@
-#sort digits in number
-num = 42145
-arr = [int(x) for x in str(num)]
+#sort digits in some number
+def main ():
+    num = 42145
+    arr = [int(x) for x in str(num)]
 
-stringoutp = ''.join(str(e) for e in sorted(arr, reverse = True))
-print(int(stringoutp))
+    stringoutp = ''.join(str(e) for e in sorted(arr, reverse = True))
+    print(int(stringoutp))
+
+if __name__ == "__main__":
+    main()
