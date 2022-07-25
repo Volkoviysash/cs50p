576d418bcaa0e93f062e2e2b195b7ec05afa23b2
Volkoviysash <volkoviysash@gmail.com>
2022-07-25 16:45:06 +0300

    Add pytest library and modernize code

--- test_twttr.py
+++ test_twttr.py
@@ -1,17 +1,6 @@
 from twttr import shorten
 
-def main():
-    test_twttr()
-
 def test_twttr():
-    if shorten("twitter") != "twttr":
-        print("Twitter error!")
-    if shorten("What's your name?") != "Wht's yr nm?":
-        print("'What's your name?' error!")
-    if shorten("CS50") != "CS50":
-        print("CS50 error!")
-    else:
-        print("All is good!")
-
-if __name__ == "__main__":
-    main()
+    assert shorten("twitter") == "twttr"
+    assert shorten("What's your name?") == "Wht's yr nm?"
+    assert shorten("CS50") == "CS50"
