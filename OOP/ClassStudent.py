a90ec4e4c8c0974f6b52bb6a14aa07ea7b08885b
Alexandr Volkovich <volkoviysash@gmail.com>
2022-10-29 22:56:54 +0300

    Create class student

---
+++ OOP/ClassStudent.py
@@ -0,0 +1,34 @@
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")        #if there is not name, return an error!                    
+        self.name = name        
+    
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    #Getter
+    @property
+    def house(self):
+        return self.house
+
+    #Setter
+    @house.setter
+    def house(self, house):
+        if house in ["Gryffindor", "Riverdale", "Lolaland"]:
+            self.house = house
+
+
+def main():
+    student = get_student()        
+    print(student)
+
+
+def get_student():        
+    name = input("Name: ")
+    house = input("House: ")     
+    return Student(name, house)
+
+        
+if __name__ == "__main__":
+    main()
