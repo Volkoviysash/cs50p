bfba7ea98cb119e1de48f5f12f6e18773413d015
Alexandr Volkovich <volkoviysash@gmail.com>
2022-10-31 22:16:25 +0300

    Update deep.py

--- deep.py
+++ deep.py
@@ -1,7 +1,7 @@
-inpt = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+input_ = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
 answers = ["42", "fourty two", "fourty_two", "fourty-two"]
 
-if inpt in answers:
+if input_ in answers:
     print("Yes")
 else:
     print("No")
